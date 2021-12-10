5e3af802ceb12a4b3aaa092c13ea72545a1a5eb0
maximbaran <43070955+maximbaran@users.noreply.github.com>
2021-12-10 12:58:42 +0200

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
 #Maxim Baran 2021-11-30
+echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
 sudo apt update
 sudo apt install sshpass
 sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@192.168.100.10 cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
